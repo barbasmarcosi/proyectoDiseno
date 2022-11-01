004d4ce30bb7fb7a90840ebfd873d2dc27cfcf23
Ian Luis Noa <noaianluis@gmail.com>
2022-11-01 00:30:56 -0300

    Ajuste Mensaje Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,43 +4,9 @@ import homePhoto from "./../assets/img/Fondo_Home.jpg";
 const Home = () => {
   return (
     <>
-      <img
-        src={homePhoto}
-        style={{
-          position: "absolute",
-          width: "100%",
-          opacity: "15%",
-          zIndex: "-1000",
-        }}
-        alt="Home"
-      ></img>
-      {/*<img
-        src={homePhoto}
-        alt="Home"
-        style={{
-          position: "absolute",
-          top: "8rem",
-          borderRadius: "15%",
-          margin: "32px",
-          backgroundClip: "padding-box",
-          border: "10px solid rgb(175, 10, 10)",
-        }}
-    ></img>*/}
+      <img className="home-image" src={homePhoto} alt="Home" ></img>
       {
-        <h1
-          style={{
-            display: "flex",
-            alignContent: "center",
-            alignItems: "center",
-            zIndex: "1000",
-            marginLeft: "3rem",
-            fontSize: "4rem",
-            width: "50%",
-            textShadow: "0 0 5px rgb(175, 10, 10)",
-            textAlign: "center",
-          }}
-          className="Home-content"
-        >
+        <h1 className="Home-content" >
           Bienvenido al Sistema de Gestion de la Empanaderia
         </h1>
       }
