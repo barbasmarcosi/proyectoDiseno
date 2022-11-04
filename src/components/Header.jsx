c50fcf85952384241318538095da893c4b8be394
Ian Luis Noa <noaianluis@gmail.com>
2022-11-04 09:37:18 -0300

    hamburguer menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,18 @@ import Logo from '../assets/img/Logo.jpg'
 const Header = () => {
   return (
     <div className='Header'>
+      
+      <div className="Header-content">
         <Link className='Header-link' to='/'>
           <img className='Logo-image' src={Logo} alt="Imagen del Logo" />
         </Link>
         <h1 className='Header-title'>La empanaderia</h1>
+      </div>
+      <div className='burger'>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     </div>
   )
 }
