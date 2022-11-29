83f8a0f495c3a24c3f46969fcaa9790570749611
Marcos Ireneo Barbas <barbasmarcosi@gmail.com>
2022-11-29 12:35:15 -0300

    feat(Estadistica): Realizada la posibilidad de determinar estadisticas de venta

--- src/containers/Estadistica.jsx
+++ src/containers/Estadistica.jsx
@@ -1,173 +1,179 @@
-import React, { useState, useRef } from "react";
-import LabeledInput from "../components/LabeledInput";
-import LabeledDataList from "../components/LabeledDataList";
-import Switch from "../components/Switch";
-import Form from "../components/Form";
-import Message from "../components/Message";
-import Modal from "../components/Modal";
+import React, { useState } from "react";
 import Table from "../components/Table";
-import useInputValue from "../hooks/useInputValue";
-import Labeler from "../components/Labeler";
-import initialState from "../initialState/initialState";
-import formValidator from "../usefullFunctions/formValidator";
-import messageTimeOut from "../usefullFunctions/messageTimeOut";
-import toGetFormValues from "../usefullFunctions/toGetFormValues";
-import ClientesAdd from "./Clientes/ClientesAdd";
 import manageLocalStorage from "../usefullFunctions/manageLocalStorage";
-import { useEffect } from "react";
 
 const Estadistica = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const [openClientModal, setOpenClientModal] = useState(false);
-  const [succesMessage, setSuccessMessage] = useState(true);
-  const [openModifyModal, setOpenModifyModal] = useState(false);
-  const [message, setMessage] = useState("");
-  const [detailsModal, setDetailsModal] = useState(false);
-  const cliente = useInputValue("");
-  const personal = useInputValue("");
-  const fechaHoraPedido = useInputValue(
-    new Date(Date.now()).toISOString().slice(0, 16)
-  );
-  const fechaHoraEnvio = useInputValue("");
-  const presupuesto = useInputValue("");
-  const estado = useInputValue("");
-  const [entregaDomicilio, setEntregaDomcilio] = useState(false);
-  const costoEnvio = useInputValue("");
-  const razonSocialEntrega = useInputValue("");
-  const telefonoEntrega = useInputValue("");
-  const delivery = useInputValue("");
-  const calle = useInputValue("");
-  const altura = useInputValue("");
-  const depto = useInputValue("");
-  const calleCliente = useInputValue("");
-  const alturaCliente = useInputValue("");
-  const deptoCliente = useInputValue("");
-  const telefonoCliente = useInputValue("");
-  const tipoVenta = useInputValue("");
-  const observaciones = useInputValue("");
-  const montoTotal = useInputValue("");
-  const producto = useInputValue("");
-  const cantidadProducto = useInputValue("");
-  const unidadMedida = useInputValue("");
-  const precioUnitario = useInputValue("");
-  const importe = useInputValue("");
-  const detailRef = useRef();
-  const clienteRef = useRef();
-  const [messageType, setMessageType] = useState("success");
   const entity = "pedido";
   const [pedidos, setPedidos] = useState(manageLocalStorage("get", entity));
+  const [pedidosCopy, setPedidosCopy] = useState(pedidos);
+  const [exceptions, setExceptions] = useState([
+    "encargado",
+    "tipoVenta",
+    "observaciones",
+    "fechaPedido",
+    "horaPedido",
+    "delivery",
+    "costoEnvio",
+  ]);
 
-  useEffect(() => {}, [pedidos]);
-
-  const toGetValue = (index) =>
-    detailRef.current.children[index].children[1].children[0].value;
-
-  let detallePedido = localStorage.getItem("detallePedido")
-    ? JSON.parse(localStorage.getItem("detallePedido"))
-    : [];
-
-  const handleAcceptButton = () => {
-    setOpenModal(!openModal);
-    setMessageType("success");
-    setMessage("Pedido agregado correctamente");
-    setSuccessMessage(false);
-    setTimeout(() => {
-      setSuccessMessage(true);
-    }, 5000);
-  };
-  const handleAcceptIngredientButton = (multiple = false) => {
-    const valid = formValidator(
-      setDetailsModal,
-      setMessage,
-      setMessageType,
-      setSuccessMessage,
-      "Producto agregado correctamente",
-      detailRef,
-      multiple
-    );
-    if (valid) {
-      let index = 0;
-      detallePedido.push({
-        producto: toGetFormValues(index++, detailRef),
-        cantidad: toGetFormValues(index++, detailRef),
-        unidadMedida: toGetFormValues(index++, detailRef),
-        precioUnitario: toGetFormValues(index, detailRef),
-        importe: toGetFormValues(1, detailRef) * toGetFormValues(3, detailRef),
+  const filterFunction = (field, newField) => {
+    const firstExtract = pedidos.map((pedido) => {
+      return {
+        [field]: pedido[field],
+        montoTotal: pedido.montoTotal,
+      };
+    });
+    const arr = [];
+    firstExtract.map((item) => {
+      let total = 0;
+      let count = 0;
+      firstExtract.map((item2) => {
+        if (item[field] === item2[field]) {
+          total += item2.montoTotal;
+          count++;
+        }
       });
-      localStorage.setItem("detallePedido", JSON.stringify(detallePedido));
-      detallePedido = JSON.parse(localStorage.getItem("detallePedido"));
-      producto.setValue("");
-      cantidadProducto.setValue("");
-      unidadMedida.setValue("");
-      precioUnitario.setValue("");
-      importe.setValue("");
-    }
+      const alreadyExists = [
+        ...new Set(
+          arr.map((element) => {
+            return element[newField] !== item[field];
+          })
+        ),
+      ];
+      if (
+        (alreadyExists[0] && alreadyExists.length < 2) ||
+        !alreadyExists.length
+      ) {
+        arr.push({
+          [newField]: item[field],
+          montoTotal: total,
+          cantidad: count,
+        });
+      }
+    });
+    setExceptions([]);
+    setPedidosCopy(arr.sort((a, b) => b.cantidad - a.cantidad));
   };
 
-  const setClientData = () => {
-    const val = clienteRef.current.value;
-    console.log(val);
-    if (val) {
-      const data = val.split(", ");
-      console.log(data);
-      data[0] !== "null" ? cliente.setValue(data[0]) : cliente.setValue("");
-      data[1] !== "null"
-        ? calleCliente.setValue(data[1])
-        : calleCliente.setValue("");
-      data[2] !== "null"
-        ? alturaCliente.setValue(data[2])
-        : alturaCliente.setValue("");
-      data[3] !== "null"
-        ? deptoCliente.setValue(data[3])
-        : deptoCliente.setValue("");
-      data[4] !== "null"
-        ? telefonoCliente.setValue(data[4])
-        : telefonoCliente.setValue("");
-    } else {
-      calleCliente.setValue("");
-      alturaCliente.setValue("");
-      deptoCliente.setValue("");
-      telefonoCliente.setValue("");
+  const doFilter = (value) => {
+    if (value === "monthFilter") {
+      const months = pedidos.map((pedido) => {
+        const arr = pedido.fechaPedido.split(/\//);
+        return { mes: `${arr[1]}-${arr[2]}`, montoTotal: pedido.montoTotal };
+      });
+      const arr = [];
+      months.map((month) => {
+        let total = 0;
+        let count = 0;
+        months.map((month2) => {
+          if (month.mes === month2.mes) {
+            total += month2.montoTotal;
+            count++;
+          }
+        });
+        const alreadyExists = [
+          ...new Set(
+            arr.map((element) => {
+              return element.mes !== month.mes;
+            })
+          ),
+        ];
+        if (
+          (alreadyExists[0] && alreadyExists.length < 2) ||
+          !alreadyExists.length
+        ) {
+          arr.push({ mes: month.mes, montoTotal: total, cantidad: count });
+        }
+      });
+      setExceptions([]);
+      setPedidosCopy(arr.sort((a, b) => b.cantidad - a.cantidad));
     }
-  };
-
-  const handleModifyButton = () => {
-    setOpenModifyModal(!openModifyModal);
-    setMessage("Pedido modificado correctamente");
-    setSuccessMessage(false);
-    messageTimeOut(setSuccessMessage);
+    if (value === "default") setPedidosCopy(pedidos);
+    if (value === "payment") filterFunction("formaPago", "metodoDePago");
+    if (value === "topSellers") {
+      const products = pedidos.map((pedido) =>
+        pedido.productos.map((product) => ({
+          producto: product.producto,
+          cantidad: product.cantidad,
+          precio: product.precioUnitario,
+        }))
+      );
+      const arr = [];
+      products.map((product) => {
+        product.map((item) => {
+          const arr2 = [];
+          let quantity = 0;
+          products.map((product2) => {
+            product2.map((item2) => {
+              if (item2.producto === item.producto) {
+                quantity += item2.cantidad;
+              }
+            });
+          });
+          let count = 0;
+          arr2.push({
+            producto: item.producto,
+            cantidad: quantity,
+            precio: item.precio,
+          });
+          arr2.map((el2) => {
+            arr.map((el) => {
+              if (el.producto === el2.producto) {
+                count++;
+              }
+            });
+          });
+          if (!count || !arr.length) {
+            arr.push({
+              producto: item.producto,
+              cantidad: quantity,
+              precio: item.precio,
+              total: quantity * item.precio,
+            });
+          }
+        });
+      });
+      setExceptions([]);
+      setPedidosCopy(arr.sort((a, b) => b.cantidad - a.cantidad));
+    }
+    if (value === "topClients") filterFunction("cliente", "cliente");
+    if (value === "sellType") filterFunction("tipoVenta", "tipoDeVenta");
+    if (value === "sellDate") filterFunction("fechaPedido", "fechaDePedido");
+    if (value === "seller") filterFunction("encargado", "vendedor");
+    if (value === "delivery") filterFunction("delivery", "delivery");
   };
 
   return (
     <div className="Productos">
       <h1 className="Productos-title">Estadisticas de venta</h1>
-      <Table
-        onDelete={(id) =>
-          setPedidos(manageLocalStorage("delete", entity, "", id))
-        }
-        onEdit={() => setOpenModifyModal(!openModifyModal)}
-        body={pedidos}
-        searchingFor={["presupuesto"]}
-        exceptions={[
-          "encargado",
-          "tipoVenta",
-          "observaciones",
-          "fechaPedido",
-          "horaPedido",
-          "delivery",
-          "costoEnvio",
-        ]}
-        isDocument={true}
-        edit={false}
-        del={false}
-      />
-      <Message
-        type={messageType}
-        hide={succesMessage}
-        onCLickClose={() => setSuccessMessage(!succesMessage)}
+      <select
+        style={{
+          height: "2rem",
+          width: "20rem",
+          marginBottom: "1rem",
+          borderRadius: "0.5rem",
+          textAlign: "center",
+          fontSize: "1rem",
+        }}
+        onChange={(e) => doFilter(e.target.value)}
       >
-        {message}
-      </Message>
+        <option value="default">Seleccione la estadistica</option>
+        <option value="monthFilter">Ventas por mes</option>
+        <option value="payment">Por metodos de pago</option>
+        <option value="topSellers">Producto mas vendido</option>
+        <option value="topClients">Cliente mas recurrente</option>
+        <option value="sellType">Por tipo de venta</option>
+        <option value="sellDate">Por fecha de pedido</option>
+        <option value="seller">Por vendedor</option>
+        <option value="delivery">Por delivery</option>
+      </select>
+      <Table
+        body={pedidosCopy}
+        edit={true}
+        del={true}
+        searchingFor={["presupuesto"]}
+        exceptions={exceptions}
+      />
     </div>
   );
 };
